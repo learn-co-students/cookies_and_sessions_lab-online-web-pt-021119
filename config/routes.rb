d18b4b8522cb9e4a6a69da4795f8341ff7cb2d66
cavu757 <jose.pehovazdiez@gmail.com>
2019-08-04 05:04:14 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/', to: 'products#index'
+  post '/add', to: 'products#add'
+  get '/clear-list', to: 'products#clear_list', via: [:destroy], as: :clear_list
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
